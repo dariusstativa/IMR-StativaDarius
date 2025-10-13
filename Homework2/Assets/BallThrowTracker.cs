55d1d15e23648b567ed2678500f9496bbd1783f0
GociuRadu <radugociu89@gmail.com>
2025-10-13 19:51:15 +0300

    h2

---
+++ Homework2/Assets/BallThrowTracker.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRGrabInteractable), typeof(Rigidbody))]
+public class BallThrowTracker : MonoBehaviour
+{
+    public Vector3 releasePosition;
+    public Vector3 releaseVelocity;
+
+    XRGrabInteractable grab;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        grab = GetComponent<XRGrabInteractable>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()
+    {
+        grab.selectExited.AddListener(OnReleased);
+    }
+    void OnDisable()
+    {
+        grab.selectExited.RemoveListener(OnReleased);
+    }
+
+    void OnReleased(SelectExitEventArgs _)
+    {
+        releasePosition = transform.position;
+        releaseVelocity = rb.velocity;
+    }
+}
